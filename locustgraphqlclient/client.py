1eaa0ecf8c25d40925ea579f0bf722ca942a6562
DesignrKnight <notify.abelmathew@gmail.com>
2020-07-13 16:08:52 +0530

    Fix

--- locustgraphqlclient/client.py
+++ locustgraphqlclient/client.py
@@ -31,7 +31,7 @@ class MeasuredGraphQLClient(GraphQLClient):
         return result
 
 
-class GraphQLLocust(Locust):
+class GraphQLLocust(User):
     def __init__(self, *args, **kwargs):
         super(GraphQLLocust, self).__init__(*args, **kwargs)
         self.client = MeasuredGraphQLClient(endpoint=self.host)
